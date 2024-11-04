ef0ddba8792847bb0fa5360a028ce139ac6d33ba
Presiyan <presiyan.prodanov@rwth-aachen.de>
2024-11-04 23:21:30 +0100

    add basics

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('stations/', views.stations, name='stations'),
+    path('analysis/', views.analysis, name='analysis'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'frontend/home.html')
+    return render(request, 'frontend/table.html')
+
+def stations(request):
+    return render(request, 'frontend/stations.html')
+
+def analysis(request):
+    return render(request, 'frontend/analysis.html')
