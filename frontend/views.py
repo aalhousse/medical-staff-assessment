b08fcb21b5ec2acfb028d86725ac2cd518008326
Felix Thape <felix.thape@gmail.com>
2024-11-07 23:51:55 +0100

    add stations view

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from backend.models import Station
 
 
 def home(request):
     return render(request, 'frontend/home.html')
+
+
+def stations(request):
+    # all_stations = Station.objects.all()  # Retrieve all stations from the database
+    all_stations = [
+        Station(station_name=f'Station {i}', is_intensive_care=False, is_child_care_unit=False,
+                patients_per_caregiver_ratio=1.5 + i * 0.1)
+        for i in range(1, 11)
+    ]
+
+    context = {
+        'stations': all_stations
+    }
+    return render(request, 'frontend/stations.html', context)
