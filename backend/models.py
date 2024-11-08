c1439adc8fb3e558ac2120fe76ccfae63f65b926
Felix Thape <felix.thape@gmail.com>
2024-11-08 17:50:22 +0100

    update models and add default data

--- backend/models.py
+++ backend/models.py
@@ -2,22 +2,47 @@
 from django.db import models
 
 
-class CareServices(models.Model):
-    """Questions and their belonging Service Classifications according to the PPBV."""
+class CareServiceField(models.Model):
+    """The Field for CareServices often abbreviated with a short name like A or S"""
 
-    care_service_name = models.CharField(max_length=200, primary_key=True)
-    care_service_category = models.CharField(max_length=1)  # For example A for 'Allgemeine Pflege'
-    care_service_task = models.CharField(max_length=50)  # For example 'Körperpflege'
-    care_service_range = models.IntegerField()  # 1-4
-    care_service_option = models.IntegerField()  # Index of the questions
-    care_service_description = models.TextField()  # Content of the question
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=128)  # For example 'Allgemeine Pflege'
+    short = models.CharField(max_length=8)  # For example 'A'
+
+    def __str__(self):
+        return f"{self.name} ({self.short})"
+
+
+class CareServiceCategory(models.Model):
+    """The Category for a CareService whether the Services regard e.g. hygiene, nutrition or mobilisation"""
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)  # The name for the Category
+
+    def __str__(self):
+        return self.name
+
+
+class CareServiceOption(models.Model):
+    """Questions and their belonging to a CareServiceField, CareServiceCategory and their position within that
+     according to the PPBV."""
+
+    id = models.IntegerField(primary_key=True)
+    field = models.ForeignKey('CareServiceField', on_delete=models.CASCADE)
+    severity = models.IntegerField()  # 1-4 the degree of help need, higher means more help
+    category = models.ForeignKey('CareServiceCategory', on_delete=models.CASCADE)
+    list_index = models.IntegerField()  # Index of the questions within the filed, category and severity list
+    description = models.TextField()  # Content of the question
+
+    def __str__(self):
+        return f"{self.field.short}{self.severity}-{self.category}-{self.list_index}"
 
 
 class DailyClassification(models.Model):
     """Daily Classification of the care services for a person."""
 
     patient_id = models.ForeignKey('Patient', on_delete=models.CASCADE)
-    classification_date = models.DateField()
+    classification_date = models.DateField()  # The date for which the classification is made
     is_in_isolation = models.BooleanField()
     data_accepted = models.BooleanField()  # Did the caregiver accept previous data
     result_minutes = models.IntegerField()  # Time in minutes calculated according to the PPBV
@@ -25,26 +50,47 @@ class DailyClassification(models.Model):
     room_name = models.CharField(max_length=100)
     bed_number = models.CharField(max_length=100)
 
+    class Meta:
+        unique_together = ('patient_id', 'classification_date')
 
-class CareServiceClassification(models.Model):
-    """Care Service Classification for a person on a specific date."""
+    def __str__(self):
+        return f"{self.patient_id} ({self.classification_date})"
 
-    care_service_name = models.ForeignKey('CareServices', on_delete=models.CASCADE)
+
+class IsCareServiceUsed(models.Model):
+    """CareServices used for a patient for the Classification on a specific date.
+    Any entry here means that the service is used"""
+
+    care_service = models.ForeignKey('CareServiceOption', on_delete=models.CASCADE)
     classification = models.ForeignKey('DailyClassification', on_delete=models.CASCADE)
+
+    class Meta:
+        # Combined primary key
+        unique_together = ('care_service', 'classification')
+
+    def __str__(self):
+        return f"{self.care_service} {self.classification}"
 
 
 class Station(models.Model):
     """Stations in the hospital."""
 
-    station_name = models.CharField(max_length=100)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
     is_intensive_care = models.BooleanField()
     is_child_care_unit = models.BooleanField()
     patients_per_caregiver_ratio = models.FloatField()
+
+    def __str__(self):
+        return self.name
 
 
 class Patient(models.Model):
     """Patients in the database."""
 
+    id = models.IntegerField(primary_key=True)  # The patients ID provided by the hospital
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    patient_id = models.CharField(max_length=20)  # The patients ID provided by the hospital
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
