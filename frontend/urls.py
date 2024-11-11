c50717c38c5b2c25f7eb1b36a709632c968ebbeb
Felix Thape <felix.thape@gmail.com>
2024-11-12 00:45:37 +0100

    add templates and urls for views

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('analysis', views.analysis, name='analysis'),
     path('stations', views.stations, name='stations'),
+    path('stations/<int:id>', views.station_patient_list, name='station_patient_list'),
+    path('stations/<int:id>/<int:patient_id>/<str:date>', views.classification, name='patient_daily_classification'),
 ]
